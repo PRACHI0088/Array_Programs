473c225c09be361e13b2ec6de72ec8f0f7a95f29
PRACHI0088 <99760316+PRACHI0088@users.noreply.github.com>
2023-03-02 01:46:21 +0530

    Add files via upload

---
+++ sum_of_arry_ele_using_func.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+ int func(int a[],int n);
+ 
+ int main()
+ {
+ 	 int n,a[10],sum=0;
+   
+   cout<<"enter n"<<endl;
+   cin>>n;
+   cout<<"now enter the array elements:"<<endl;
+   for(int i=0;i<n;i++)
+   {
+   	cin>>a[i];
+   }
+   sum=func(a,n);
+   cout<<"The sum of the array elements is:"<<sum;
+   return 0;
+ }
+ 
+ int func(int a[],int n)
+ {  int s;
+ 	 for(int i=0;i<n;i++)
+ 	 s=s+a[i];
+     return s;
+ }
