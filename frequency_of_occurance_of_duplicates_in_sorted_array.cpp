d2d09afe3dd1cab7fb5c0e0a13631bdfcb20786b
PRACHI <99760316+PRACHI0088@users.noreply.github.com>
2023-08-26 02:22:11 +0530

    Add files via upload

---
+++ frequency_of_occurance_of_duplicates_in_sorted_array.cpp
@@ -0,0 +1,31 @@
+//finding duplicate elements in a SORTED ARRAY and Counting their no. of duplicates.
+#include<iostream>
+using namespace std;
+int main()
+{
+		int a[50],n,j,no;
+	cout<<"enter the size of the array: \n";
+		cin>>n;
+		cout<<"enter the  array elements in SORTED manner:\n";
+		for(int i=0;i<n;i++)
+		{
+			cin>>a[i];
+		}
+		
+		for(int i=0;i<n-1;i++)
+		{
+			if(a[i]==a[i+1])
+			{
+				j=i+1;
+				while(a[j]==a[i])
+				{
+					j++;
+				}
+				no=j-i;
+				cout<<"The number  "<<a[i]<<"  occured  "<<no<<"  Times"<<endl;
+				i=j-1;
+			}
+		}
+
+return 0;
+}
