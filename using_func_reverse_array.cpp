2e058d3748ab5c55b2a52c141f6ec7480aa53ddc
PRACHI <99760316+PRACHI0088@users.noreply.github.com>
2023-08-29 03:29:41 +0530

    Add files via upload

---
+++ using_func_reverse_array.cpp
@@ -0,0 +1,63 @@
+ #include<iostream>
+using namespace std;
+ 
+ void inputarray(int a[],int n)
+ {
+ 	 for(int i=0;i<n;i++)
+   {
+   	cin>>a[i];
+   }
+ }
+ 
+ void printarray(int a[],int n)
+ {
+ 	 for(int i=0;i<n;i++)
+   {
+   	cout<<a[i]<<endl;
+   }
+ }
+
+ 
+ void revarray(int a[],int n)
+ { int s=0,e=n-1;   //s=starting element, e=last element
+ while(s<=e)
+ {  
+       	  swap(a[s],a[e]);  //INBULIT FUNCTION: to swap two array elements
+	   	  s++;
+       	  e--;
+		}	
+ }
+ 	
+ 
+ int main()
+ {
+ 	 int n,a[10];
+   
+   cout<<"enter n, number of array elements:"<<endl;
+   cin>>n;
+   cout<<"now enter the array elements:"<<endl;
+   inputarray(a,n);
+    cout<<"the reversed array is:"<<endl;
+    revarray(a,n);
+    
+ printarray(a,n);
+   
+   return 0;
+}
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
