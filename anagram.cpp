beccf65527db47e8446486061399266007d4fe17
PRACHI <99760316+PRACHI0088@users.noreply.github.com>
2023-09-20 22:38:19 +0530

    Add files via upload

---
+++ anagram.cpp
@@ -0,0 +1,32 @@
+// check if two strings are ANAGRAM or not.
+////COMPLETE IT BROOOOOOO!!!!!
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+int main()
+{ char a[50],b[50];
+int flag=1;
+  unordered_map<char,int> map;
+  
+  cout<<"enter the first string:"<<endl;
+  gets(a);
+  cout<<"enter the second string:"<<endl;
+  gets(b);
+  for(int i=0;a[i]!='\0';i++)
+  {
+  	map[a[i]]++;
+  }
+  for(int i=0;b[i]!='\0';i++)
+  {
+  	map[b[i]]--;
+  	if(map[b[i]]<0)
+  	{
+  		cout<<"Not Anagram Strings !"<<endl;
+		  break;	
+	}
+  }
+  	cout<<"The Strings are Anagram "<<endl;
+
+ return 0;
+}
